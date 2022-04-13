e55c1f44fe68dbb33e2fca674f69724b37dbee23
PhilouLaudun <77538675+PhilouLaudun@users.noreply.github.com>
2022-04-13 19:43:57 +0200

    Mise en forme page Hotel: finalisation zone suite

--- src/componants/DetailHotel.js
+++ src/componants/DetailHotel.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react'
-import { DetailHotelData } from '../data/detailHotelData';
-import { DetailChambreData } from '../data/detailchambreData';
+import React, { useState } from "react";
+import { DetailHotelData } from "../data/detailHotelData";
+import { DetailChambreData } from "../data/detailchambreData";
 
 const DetailHotel = (props) => {
-    const [currentHotel] = useState(DetailHotelData)
-    const hotel = currentHotel[props.numeroHotel];
+  const [currentHotel] = useState(DetailHotelData);
+  const hotel = currentHotel[props.numeroHotel];
+  const [suite] = useState(DetailChambreData);
+  const numberHotel = props.numeroHotel + 1;
+  const array = suite.filter((tri) => tri.hotel === numberHotel);
   return (
     <div className="hotelcontent">
       <div className="nom_hotel">
@@ -19,12 +22,20 @@ const DetailHotel = (props) => {
 
       <div className="suitehotel">
         <h2>Les suites</h2>
-<h2>Les suites</h2>
+        {array.map((chambre) => (
+          <section className="suite">
+            <div className="suitecarte">
+              <div className="imagecarte">
+                <img src={chambre.img} key={chambre.id} alt="" />
+              </div>
 
+              <h3>{chambre.nom}</h3>
+            </div>
+          </section>
+        ))}
       </div>
-
     </div>
   );
-}
+};
 
-export default DetailHotel
+export default DetailHotel;
